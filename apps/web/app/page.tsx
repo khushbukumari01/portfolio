6033cfb63ea5aa44934ec9bd4561a116b44a8ab9
Khushbu Kumari <khushbu.kumari.ece22@itbhu.ac.in>
2025-03-28 00:44:20 +0530

    added experience

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -49,224 +49,23 @@ export default function Page(): JSX.Element {
 
   useEffect(() => {
     const internshipData = [
-      {
-        id: 1,
-        company: "DivineTalk",
-        position: "Software Development Intern",
-        description:
-          "Worked on a team to develop a web application using React and Node.js",
-        startDate: "2020-05-01",
-        endDate: "2020-08-31",
-        skills: ["React", "Node.js", "JavaScript"],
-        image:
-          "https://media.licdn.com/dms/image/v2/D4D0BAQEMi_iL1AX2OA/company-logo_100_100/company-logo_100_100/0/1705392209325/divinetalk_logo?e=1746662400&v=beta&t=9wEKzpYoks5yFn6Xz3KRtSxqTfwHndTU0dDB0iop9YQ",
-      },
-      {
-        id: 2,
-        company: "Company B",
-        position: "Data Science Intern",
-        description:
-          "Worked on a team to develop a machine learning model using Python and TensorFlow",
-        startDate: "2021-06-01",
-        endDate: "2021-09-30",
-        skills: ["Python", "TensorFlow", "Machine Learning"],
-        image: "https://picsum.photos/id/201/200/200",
-      },
-      {
-        id: 3,
-        company: "Company C",
-        position: "Cybersecurity Intern",
-        description:
-          "Worked on a team to develop a security system using C++ and Linux",
-        startDate: "2022-01-01",
-        endDate: "2022-04-30",
-        skills: ["C++", "Linux", "Cybersecurity"],
-        image: "https://picsum.photos/id/301/200/200",
-      },
     ];
 
     const experienceData = [
       {
         id: 1,
-        company: "DivineTalk Astrology",
-        position: "Software Engineer",
+        company: "Mashal",
+        position: "Sports Co-Cordinator",
         description:
-          "Worked on various projects and contributed to software development.",
-        startDate: "Dec 2024",
+          "Worked on managing the games and multiple teams.",
+        startDate: "Dec 2023",
         endDate: "Present",
         skills: [
-          "Amazon Web Services (AWS)",
-          "Express.js",
-          "Kibana",
-          "Elasticsearch",
-          "MySQL",
-          "Elastic Stack (ELK)",
+          "Team Management",
+          "Leadership",
         ],
         image:
-          "https://media.licdn.com/dms/image/v2/D4D0BAQEMi_iL1AX2OA/company-logo_100_100/company-logo_100_100/0/1705392209325/divinetalk_logo?e=1746662400&v=beta&t=9wEKzpYoks5yFn6Xz3KRtSxqTfwHndTU0dDB0iop9YQ",
-      },
-      {
-        id: 2,
-        company: "FMC Weekend",
-        position: "Senior Advisor",
-        description:
-          "Oversaw technical operations and led event participation.",
-        startDate: "Jan 2024",
-        endDate: "Oct 2024",
-        skills: ["Leadership", "Event Management"],
-        image:
-          "https://media.licdn.com/dms/image/v2/D560BAQH86tUgFign6Q/img-crop_100/img-crop_100/0/1723556307899?e=1746662400&v=beta&t=utKewCX3wl_PKxOslwBAiBDXuKW3cVuwFhzh96QDQYg",
-      },
-      {
-        id: 3,
-        company: "FMC Weekend",
-        position: "Tech Head",
-        description:
-          "Led technical operations, increasing event participation by 30%.",
-        startDate: "May 2023",
-        endDate: "Dec 2023",
-        skills: ["Leadership", "Technical Operations"],
-        image:
-          "https://media.licdn.com/dms/image/v2/D560BAQH86tUgFign6Q/img-crop_100/img-crop_100/0/1723556307899?e=1746662400&v=beta&t=utKewCX3wl_PKxOslwBAiBDXuKW3cVuwFhzh96QDQYg",
-      },
-      {
-        id: 4,
-        company: "FMC Weekend",
-        position: "Technical Executive",
-        description: "Contributed to various technical projects.",
-        startDate: "Nov 2022",
-        endDate: "Jun 2023",
-        skills: ["Node.js", "Front-End Development", "Back-End Development"],
-        image:
-          "https://media.licdn.com/dms/image/v2/D560BAQH86tUgFign6Q/img-crop_100/img-crop_100/0/1723556307899?e=1746662400&v=beta&t=utKewCX3wl_PKxOslwBAiBDXuKW3cVuwFhzh96QDQYg",
-      },
-      {
-        id: 5,
-        company: "Jindal Steel & Power Ltd.",
-        position: "Technology Integration Intern",
-        description:
-          "Worked on multiple projects including volume calculations and automation.",
-        startDate: "May 2024",
-        endDate: "Jul 2024",
-        skills: ["Python", "JavaScript", "Data Visualization", "3D Modeling"],
-        image:
-          "https://media.licdn.com/dms/image/v2/C4E0BAQEq60t16uDOWw/company-logo_100_100/company-logo_100_100/0/1630628425888/jindal_steel__power_ltd__logo?e=1746662400&v=beta&t=M55XVbTsiC_Wn85PjHABEROsP8aXxCJU3sYhYclEcFk",
-      },
-      {
-        id: 6,
-        company: "Rejoy Health",
-        position: "Back End Developer Intern",
-        description: "Contributed to backend development.",
-        startDate: "May 2024",
-        endDate: "May 2024",
-        skills: ["AWS Fargate", "Amazon SQS", "Celery"],
-        image:
-          "https://media.licdn.com/dms/image/v2/C560BAQHlwA6rRf8OrA/company-logo_100_100/company-logo_100_100/0/1660842587536/rejoyhealthusa_logo?e=1746662400&v=beta&t=WyASMsiRUwrhf8JysgZhG-uHJKP6MY1kOTOFieOX5EQ",
-      },
-      {
-        id: 7,
-        company: "One Inc Cooperative",
-        position: "Software Engineer Intern",
-        description: "Worked on software engineering projects.",
-        startDate: "Apr 2024",
-        endDate: "May 2024",
-        skills: ["AWS Fargate", "Amazon SQS", "Celery"],
-        image:
-          "https://media.licdn.com/dms/image/v2/C4E0BAQEOxz-UbEqY0g/company-logo_100_100/company-logo_100_100/0/1630638304030/citizensofone_logo?e=1746662400&v=beta&t=xkSA47SsSC-D5aOUnostW7TfHU0uu1Waic7wfU2hEBQ",
-      },
-      {
-        id: 8,
-        company: "JS Tigers",
-        position: "Software Developer Intern",
-        description: "Worked on various software development projects.",
-        startDate: "Apr 2024",
-        endDate: "May 2024",
-        skills: [
-          "APIless Full Stack Website",
-          "tRPC",
-          "Material-UI",
-          "Tailwind CSS",
-          "React Native",
-        ],
-        image:
-          "https://media.licdn.com/dms/image/v2/D560BAQFWw6cuyDYnbw/company-logo_100_100/company-logo_100_100/0/1711082676203/jstigers_logo?e=1746662400&v=beta&t=7766R82PmUJ5zVSNN-L2IcPGI-hbcJ6oppIJ8uNN10U",
-      },
-      {
-        id: 9,
-        company: "Acencore",
-        position: "Full Stack Developer Intern",
-        description: "Led CI/CD implementation and backend development.",
-        startDate: "Mar 2024",
-        endDate: "Apr 2024",
-        skills: ["Docker", "GitHub Actions", "NestJS", "FastAPI"],
-        image:
-          "https://media.licdn.com/dms/image/v2/D4D0BAQE1ShqDr0dkLg/company-logo_100_100/company-logo_100_100/0/1710004323468/acencoreai_logo?e=1746662400&v=beta&t=Iv8xlNLpZFf_g2Y4kn9NiEZ2tRSsGHTMZcuj7Lv9WCQ",
-      },
-      {
-        id: 10,
-        company: "Ostello AI",
-        position: "DevOps and Backend Engineer",
-        description:
-          "Contributed to backend development and DevOps initiatives.",
-        startDate: "Jan 2024",
-        endDate: "Feb 2024",
-        skills: ["NestJS", "PostgreSQL", "Redis", "Elastic Beanstalk"],
-        image:
-          "https://media.licdn.com/dms/image/v2/C4D0BAQEUOe5VKZ0Hhw/company-logo_100_100/company-logo_100_100/0/1659072964018/ostello_india_logo?e=1746662400&v=beta&t=-Asu_6cT8o6vbZy8gNvPAREv5ZeGJGCowuLRB-GqpYo",
-      },
-      {
-        id: 11,
-        company: "Techies Gateway",
-        position: "Frontend Developer Intern",
-        description: "Worked on frontend development projects.",
-        startDate: "Dec 2023",
-        endDate: "Dec 2023",
-        skills: ["Front-End Development"],
-        image:
-          "https://media.licdn.com/dms/image/v2/D560BAQGewu5KVZWVPg/company-logo_100_100/company-logo_100_100/0/1711037280811/techies_gateway_logo?e=1746662400&v=beta&t=XC7b2jzIbpW8CKcvkacAW1MoJOhqsnH4lKLp7as-w3s",
-      },
-      {
-        id: 12,
-        company: "TryAndes",
-        position: "Software Engineer Intern",
-        description: "Contributed to frontend and backend development.",
-        startDate: "Jul 2023",
-        endDate: "Oct 2023",
-        skills: ["Next.js", "Flask", "Typescript", "MySQL"],
-        image: "https://picsum.photos/id/1125/200/200",
-      },
-      {
-        id: 13,
-        company: "Club of Programmers, IIT BHU",
-        position: "Software Development Group Member",
-        description: "Worked on various software development projects.",
-        startDate: "Dec 2022",
-        endDate: "Jul 2023",
-        skills: ["Node.js", "Front-End Development", "Back-End Development"],
-        image:
-          "https://media.licdn.com/dms/image/v2/C4E0BAQFWyeIkMecCww/company-logo_100_100/company-logo_100_100/0/1630654066846/cops_iitbhu_logo?e=1746662400&v=beta&t=-ku2G86sz3Ebj7n7iK-N6eKWPLHtFr9e9QW7PAK-6f0",
-      },
-      {
-        id: 14,
-        company: "Science and Technology Council, IIT BHU",
-        position: "Technical Executive",
-        description: "Contributed to various technical projects.",
-        startDate: "Nov 2022",
-        endDate: "Mar 2023",
-        skills: ["Node.js", "Front-End Development", "Back-End Development"],
-        image:
-          "https://media.licdn.com/dms/image/v2/D560BAQH_WyK-qBHGFA/company-logo_100_100/company-logo_100_100/0/1711644358016?e=1746662400&v=beta&t=Q2u-UmBWdF_BV4MtpKG1qd26XZtK-6YWzmPCaCq9NAA",
-      },
-      {
-        id: 15,
-        company: "Filo",
-        position: "Doubt Expert",
-        description: "Provided assistance and support.",
-        startDate: "Feb 2022",
-        endDate: "May 2022",
-        skills: ["Communication", "Problem Solving"],
-        image:
-          "https://media.licdn.com/dms/image/v2/C4D0BAQFw5QXPmupPsQ/company-logo_100_100/company-logo_100_100/0/1667979005637/filoedtech_logo?e=1746662400&v=beta&t=SoYO0Dn0Uzih5CVHL5mMjue09UN2rsXDENIGnDCelCM",
+          "https://media.licdn.com/dms/image/v2/C4D0BAQHGS5PRR2WFJg/company-logo_200_200/company-logo_200_200/0/1663357474575/ees_iit_bhu_logo?e=1748476800&v=beta&t=Drpwzl-3Onyh9yCwNyEwTdyNn7CJ_O-Sl86IkV8MY-Y",
       },
     ];
 
@@ -275,26 +74,26 @@ export default function Page(): JSX.Element {
         id: 1,
         institution:
           "Indian Institute of Technology (Banaras Hindu University), Varanasi",
-        degree: "Bachelor of Technology - BTech, Mining",
-        startDate: "2021",
-        endDate: "2025",
+        degree: "Bachelor of Technology - BTech, Electronics and Communication",
+        startDate: "2022",
+        endDate: "2026",
         grade: "CGPA: 8.63",
         skills: [
           "Node.js",
-          "User Interface Design",
-          "Front-End Development",
-          "Responsive Web Design",
+          "Problem Solving",
+          "Data Structure and Algorithms",
+          "Matlab",
           "Back-End Web Development",
         ],
         logo: "https://media.licdn.com/dms/image/v2/C4D0BAQHyenVUtyQrLg/company-logo_100_100/company-logo_100_100/0/1631309569195?e=1746662400&v=beta&t=fgUDvyYmXlSDS3dhnfTkc17ZvfwaqcaV0H36qpINNEc",
       },
       {
         id: 2,
-        institution: "DAUDNAGAR COLLEGE DAUDNAGAR",
+        institution: "SS School Supaul",
         degree: "Intermediate, PCM",
-        startDate: "Apr 2018",
-        endDate: "Feb 2020",
-        grade: "District Rank (<10) State Rank (<20)",
+        startDate: "Apr 2019",
+        endDate: "Feb 2021",
+        grade: "",
         skills: [],
         logo: "https://www.daudnagarcollege.ac.in/upload/images/16729045601624091965logo.png",
       },
@@ -304,7 +103,7 @@ export default function Page(): JSX.Element {
         degree: "Matriculation",
         startDate: "Dec 2013",
         endDate: "Feb 2018",
-        grade: "Overall Bihar Rank - 14",
+        grade: "Overall Bihar Rank - 7",
         skills: [],
         logo: "https://savbihar.ac.in/wp-content/uploads/2021/04/simultala_logo.jpg",
       },
